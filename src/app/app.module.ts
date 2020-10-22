cb5db3f434722852ce06fe00fcb4284fba1a3bc2
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-22 12:44:49 +0200

    styles-v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { StartComponent } from './start/start.component';
+import { ProductCardComponent } from './shared/components/product-card/product-card.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { StartComponent } from './start/start.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    StartComponent
+    StartComponent,
+    ProductCardComponent
     
   ],
   imports: [
